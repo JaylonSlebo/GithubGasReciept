602b7b5592b894e067c7a3c35f3a6fd68f8ac2f2
Jaylon Slebo <jaylon.slebo.2023@student.lhschools.org>
2020-02-24 12:11:25 -0500

    Intelij Commit

---
+++ src/com/LickingHeights/Main.java
@@ -0,0 +1,37 @@
+package com.LickingHeights;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("+--------------------------------+");
+        System.out.println("|    Welcome to Quicky Mart      |");
+        System.out.println("|         Gas Station            |");
+        System.out.println("|         Sale Receipt           |");
+        System.out.println("|      514 Hannifin Drive,       |");
+        System.out.println("|    YourMomVille, OH 45312      |");
+        System.out.println("|                                |");
+        System.out.println("|       Date        Time         |");
+        System.out.println("|       01/01/2012  4:01PM       |");
+        System.out.println("|       Auth#    10101010        |");
+        System.out.println("|          MasterCard            |");
+        System.out.println("|        Account Number          |");
+        System.out.println("|        ************19          |");
+        System.out.println("|                                |");
+        System.out.println("|       Pump   Product   $/G     |");
+        System.out.println("|       01      UNLD     $2.90   |");
+        System.out.println("|                                |");
+        System.out.println("|      Gallons   Fuel   Total    |");
+        System.out.println("|      20.69            $60.00   |");
+        System.out.println("|                                |");
+        System.out.println("|            Thank You!          |");
+        System.out.println("|           Gas Station          |");
+        System.out.println("|            Blue Shell          |");
+        System.out.println("|                                |");
+        System.out.println("|                                |");
+        System.out.println("+--------------------------------+");
+
+
+
+        }
+}
+
